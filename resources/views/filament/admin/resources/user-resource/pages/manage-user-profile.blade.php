2c221b930318278702f5385ab384a1b90ba5ed1e
ProActiveDigiDev <hanro@proactivedigitalconcepts.co.za>
2023-12-22 15:59:05 +0200

    Created user Profile Management Custom Page

---
+++ resources/views/filament/admin/resources/user-resource/pages/manage-user-profile.blade.php
@@ -0,0 +1,86 @@
+<x-filament-panels::page>   
+
+    <div x-data="{activeTab : $wire.$entangle('activeTab') }">
+        <x-filament::tabs label="Content tabs">
+            <x-filament::tabs.item
+            :active="$activeTab === 'tab_1'"
+            wire:click="$set('activeTab', 'tab_1')"
+            >
+            Account Information
+            </x-filament::tabs.item>
+
+            <x-filament::tabs.item
+            :active="$activeTab === 'tab_2'"
+            wire:click="$set('activeTab', 'tab_2')"
+            >
+            Profile
+            </x-filament::tabs.item>
+
+            <x-filament::tabs.item
+            :active="$activeTab === 'tab_3'"
+            wire:click="$set('activeTab', 'tab_3')"
+            >
+            Medical Aid Information
+            </x-filament::tabs.item>
+
+            <x-filament::tabs.item
+            :active="$activeTab === 'tab_4'"
+            wire:click="$set('activeTab', 'tab_4')"
+            >
+            Emergency Contact Information
+            </x-filament::tabs.item>
+
+            <x-filament::tabs.item
+            :active="$activeTab === 'tab_5'"
+            wire:click="$set('activeTab', 'tab_5')"
+            >
+            Riders Information
+            </x-filament::tabs.item>
+
+            <div class="flex justify-end">
+                <x-filament::button wire:click="submit" outlined>
+                    Save Changes
+                </x-filament::button>
+            </div>
+        
+        </x-filament::tabs>
+
+        <div class="tabs-content-holder">
+
+            <div x-show="activeTab == 'tab_1'">
+                {{ $this->accountInfolist }} 
+            </div>
+
+            <div x-show="activeTab == 'tab_2'">
+                <form wire:submit.prevent="submit">
+                    @csrf
+                    {{ $this->profileForm }}
+                </form> 
+            </div>
+            
+            <div x-show="activeTab == 'tab_3'">
+                <form wire:submit.prevent="submit">
+                    @csrf
+                    {{ $this->emergencyInformationForm }}
+                </form> 
+            </div>
+            
+            <div x-show="activeTab == 'tab_4'">
+                <form wire:submit.prevent="submit">
+                    @csrf
+                    {{ $this->emergencyContactForm }}
+                </form> 
+            </div>
+
+            <div x-show="activeTab == 'tab_5'">
+                @foreach($this->riders as $rider)
+                    <a href="/filament/resources/riders/{{ $rider->id }}/edit" class="fi-btn-label">
+                        Edit {{ $rider->name }} (ID: {{ $rider->id }})
+                    </a>
+                @endforeach 
+            </div>
+
+        </div>
+    </div>
+    
+</x-filament-panels::page>
